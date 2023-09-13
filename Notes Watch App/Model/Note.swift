a8f4cef45e34a652e2eccd80f74d40ffb12cf8f7
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-13 20:32:56 +0300

    main view commit

---
+++ Notes Watch App/Model/Note.swift
@@ -0,0 +1,17 @@
+//
+//  Note.swift
+//  Notes Watch App
+//
+//  Created by Zeki Baklan on 13.09.2023.
+//
+
+import Foundation
+
+struct Note : Codable , Identifiable {
+    
+    let id : UUID
+    let text : String
+    
+    
+    
+}
--- Notes Watch App/View/ContentView.swift
+++ Notes Watch App/View/ContentView.swift
@@ -8,16 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //    MARK: - PROPERTY
+    
+    @State private var notes : [Note]  = [Note]()
+    @State private var text : String = ""
+    
+    
+    //    MARK: - FUNCTION
+    
+    func getDocumentDirectory() -> URL{
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    
+    
+    func save () {
+        dump(notes)
+    }
+    
+    
+    
+    //    MARK: - BODY
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            HStack(alignment: .center, spacing: 6) {
+                TextField("Add New Note", text: $text)
+                Button {
+                    // 1. Only run the button's action when the text field is not empty
+                    
+                    guard text.isEmpty == false else { return }
+                    // 2.Create a new note item and initialize it with the text value
+                    let note = Note(id: UUID(), text: text)
+                    
+                    // 3. Add the  new note item to the notes array(append)
+                    notes.append(note)
+                    // 4.Make the text field empty
+                    text = ""
+                    // 5.Save the notes (funtion)
+                    save()
+                        
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 42, weight: .semibold))
+                }
+                .fixedSize()
+                .buttonStyle(PlainButtonStyle())
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+                
+//                .buttonStyle(BorderedButtonStyle(tint: .accentColor))
+
+            }//:HSTACK
+            Spacer()
+            Text("\(notes.count)")
+        }//:VSTACK
+        .navigationTitle("xrert")
         .padding()
     }
 }
+
+
+ //    MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
